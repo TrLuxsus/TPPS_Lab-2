7fc45d04dc8949c1062aaafb854784036e23235e
Dima Moruha <trluxsus@gmail.com>
2020-09-21 11:08:18 +0300

    TPPS_Lab-2 Task-2.
    Create test for Fibonacci class with two tests.
    
    Signed-off-by: Dima Moruha <trluxsus@gmail.com>

---
+++ src/ki_17_5/lab_2/var_03/task_02/FibonacciTest.java
@@ -0,0 +1,18 @@
+package ki_17_5.lab_2.var_03.task_02;
+
+import org.junit.Assert;
+
+import static org.junit.Assert.fail;
+
+public class FibonacciTest {
+
+    @org.junit.Test
+    public void firstBiggerThen() {
+        fail();
+    }
+
+    @org.junit.Test
+    public void sumMembersLessThen() {
+        fail();
+    }
+}
